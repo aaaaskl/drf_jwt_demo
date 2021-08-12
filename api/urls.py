408d22182c358017abf2867c4d35df3f24fef4bd
aaaaskl <253904660@qq.com>
2021-08-12 11:50:51 +0800

    urls 注释

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,8 @@ from django.conf.urls import url, include
 from api import views
 
 urlpatterns = [
+    #asl;kdjfiasjdfl;akj
+    #dslkafgtestsetsgit
     url('^login/$', views.LoginView.as_view()),
     url('^order/$', views.OrderView.as_view()),
 ]
